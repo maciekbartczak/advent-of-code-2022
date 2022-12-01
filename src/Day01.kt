ccc5a90acfc1e4f7b925cec6e7eae17cdeb4547f
maciekbartczak <maciekbartczak17@gmail.com>
2022-12-01 22:23:52 +0100

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return input
+            .split("\n\n")
+            .maxOf { calories ->
+                calories
+                    .split("\n")
+                    .sumOf { it.toInt() }
+            }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        return input
+            .split("\n\n")
+            .map { calories ->
+                calories
+                    .split("\n")
+                    .sumOf { it.toInt() }
+            }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputAsString("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInputAsString("Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,12 +1,16 @@
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import java.util.function.Predicate
 
 /**
  * Reads lines from the given input txt file.
  */
 fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
+
+fun readInputAsString(name: String) = File("src", "$name.txt")
+    .readText()
 
 /**
  * Converts string to md5 hash.
